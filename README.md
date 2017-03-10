432cebaa341a443448d1e10a9ceab4e6a1d1b429
mariuszkowalski <mariusz.kowalski.115@gmail.com>
2017-03-10 20:28:32 +0100

    Core of the program, first window, and logic which prevents on launching on other systems than windows.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # BioCounter
 Simple and easy to use program, to count various elements in biological science.
+
+# # Goals.
+The Goal is to make the program simple and easy to use.
+I also would like to limit the use of external python modules to zero, but
+some of those may be just necessary, thus I am going to limit the occurrences to absolute minimum.
+
+# # Why Python.
+I am aware that there are a lot of more suited programing languages for building
+windows offline application than python, but it does not mean it can not be done.
+Also I have used the Tkinter and ttk, because it is perceived as obsolete and useless
+but it is far from true. There are modules like: pyQt, wxPython, kivy or pygame,
+but for such simple use the Tkinter is sufficient and it is built in into python.
---
+++ run.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+
+from tkinter import *
+from tkinter import ttk
+from tkinter.filedialog import askopenfilename
+from tkinter.filedialog import asksaveasfilename
+from tkinter import messagebox
+import codecs
+import platform
+
+
+class Window:
+
+    def __init__(self, mainWidget, screen_width, screen_height):
+        '''
+        Main Window of the program.
+        Contains all necessary elements to navigate and use all functionalities.
+
+        Args:
+            mainWidget: instance - instance of the Tk() class.
+            screen_width: int - width of the screen.
+            screen_height: int - width of the screen.
+        '''
+
+        self.screen_width = screen_width
+        self.screen_height = screen_height
+
+        self.status_bar_text = StringVar()
+        self.status_bar_text.set('')
+
+        self.main_frame = ttk.Frame(mainWidget, width=self.screen_width, height=self.screen_height-20, padding=(0, 0, 0, 0))
+        self.main_frame.place(x=0, y=0)
+
+        self.status_bar = ttk.Label(mainWidget, width=self.screen_width, anchor=W, border=0, relief=SUNKEN, textvariable=self.status_bar_text)
+        self.status_bar.place(x=0, y=self.screen_height-20)
+
+        self.main_gui()
+
+    def main_gui(self):
+        pass
+
+    def remove_elements(self, elements):
+        for element in elements:
+            element.destroy()
+
+
+def main():
+    root = Tk()
+    screen_width = root.winfo_screenwidth() - 15
+    screen_height = root.winfo_screenheight() - 70
+    root.geometry('{}x{}+5+5'.format(screen_width, screen_height))
+    root.title('Bio Counter')
+    root.wm_attributes('-topmost', 0)
+    root.resizable(width=0, height=0)
+
+    window = Window(root, screen_width, screen_height)
+    window
+
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    if platform.system() == 'Windows':
+        main()
+    else:
+        print('Platform not supported.')
